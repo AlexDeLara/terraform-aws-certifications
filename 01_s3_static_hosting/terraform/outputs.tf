4ef02d19024076fffaa0e17296974a7a2124a202
AlexDeLara <hfdelara@gmail.com>
2023-04-06 15:25:46 -0600

    Remove bucket ARN output and correct endpoint output

--- 01_s3_static_hosting/terraform/outputs.tf
+++ 01_s3_static_hosting/terraform/outputs.tf
@@ -1,7 +1,3 @@
-output "static_hosting_domain" {
-  value = aws_s3_bucket_website_configuration.static_hosting_bucket.website_domain
-}
-
-output "static_hosting_bucket_arn" {
-  value = aws_s3_bucket.static_hosting_bucket.arn
+output "static_hosting_endpoint" {
+  value = aws_s3_bucket_website_configuration.static_hosting_bucket.website_endpoint
 }
