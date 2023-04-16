624ee71c09337e7692348d39adf3d4c1b79837e7
AlexDeLara <hfdelara@gmail.com>
2023-04-16 14:51:22 -0600

    Add EC2 instance SG

--- 03_vpc_natg/terraform/data.tf
+++ 03_vpc_natg/terraform/data.tf
@@ -1,9 +1,10 @@
-data "aws_ami" "example" {
-  most_recent      = true
-  owners           = ["amazon"]
+data "aws_ami" "ami_aws_linux_latest" {
+  most_recent = true
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
     values = ["al2023-ami-2023*-kernel-*-x86_64"]
   }
 }
+
--- 03_vpc_natg/terraform/locals.tf
+++ 03_vpc_natg/terraform/locals.tf
@@ -7,4 +7,6 @@ locals {
 
   vpc_sn = flatten([for iaz in local.azs : [for itier in local.tiers : { az = "${local.region}${iaz}", name = "sn-${itier}-${upper(iaz)}", public = itier == "web" ? true : false }]])
 
+  ec2_key_name = "AH-TRAINING-AWS-GENERAL-A4L"
+
 }
